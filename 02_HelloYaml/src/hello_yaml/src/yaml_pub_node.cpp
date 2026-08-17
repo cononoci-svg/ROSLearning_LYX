4e4d9f8a873ebc8f5617a10074a38c2502a72313
Lanyuxiu <cononoci@gmail.com>
2026-08-17 15:00:22 +0800

    添加yaml配置文件并实现节点读取自定义参数

--- 02_HelloYaml/src/hello_yaml/src/yaml_pub_node.cpp
+++ 02_HelloYaml/src/hello_yaml/src/yaml_pub_node.cpp
@@ -2,28 +2,36 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
 class YamlPublisher : public rclcpp::Node {
 public:
   YamlPublisher() : Node("yaml_pub_node") {
-    pub_ = this->create_publisher<std_msgs::msg::String>("yaml_topic", 10);
+    // 声明参数
+    this->declare_parameter<float>("initial_x", 0.0);
+    this->declare_parameter<float>("initial_y", 0.0);
+    this->declare_parameter<float>("initial_z", 0.0);
+
+    // 读取参数
+    this->get_parameter("initial_x", initX_);
+    this->get_parameter("initial_y", initY_);
+    this->get_parameter("initial_z", initZ_);
+
     timer_ = this->create_wall_timer(
       2000ms, std::bind(&YamlPublisher::timer_callback, this));
   }
 
 private:
   void timer_callback() {
-    auto msg = std_msgs::msg::String();
-    msg.data = "Hello from yaml_pub_node!";
-    RCLCPP_INFO(this->get_logger(), "发布: '%s'", msg.data.c_str());
-    pub_->publish(msg);
+    RCLCPP_INFO(this->get_logger(), "从Yaml读取的初始坐标: x=%.2f, y=%.2f, z=%.2f", 
+                initX_, initY_, initZ_);
   }
 
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  float initX_;
+  float initY_;
+  float initZ_;
 };
 
 int main(int argc, char * argv[]) {
